be72075ff36314ec9d1ea842396149bbd7d7d962
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-06-04 23:10:41 -0700

    Position elements in hero layout

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,15 @@ export default function Home() {
   return (
     <>
       <BackgroundContainer color="neutral" shade="900" textColor="light">
-        <h1 className="h1">Nick Hibbits</h1>
+        <div className="hero flex flex-justify-center flex-align-center">
+          <div>
+            <h1 className="fs-1000 fw-700">Nick Hibbits</h1>
+            <h2 className="fs-800 "> Web Developer</h2>
+            <p className="fs-600">
+              Frontend engineer specializing in web-based applications
+            </p>
+          </div>
+        </div>
       </BackgroundContainer>
       <BackgroundContainer color="primary" shade="400" textColor="dark">
         <h1 className="h1">Work </h1>
