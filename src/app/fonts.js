9b4a3b5ccf117507512c312b94de44fc8b2bebc1
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-06-10 19:22:54 -0700

    include new font weights in design system

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -3,12 +3,27 @@ import localFont from "next/font/local";
 export const switzer = localFont({
   src: [
     {
-      path: "../../public/fonts/switzer/Fonts/WEB/fonts/Switzer-Regular.woff2",
+      path: "../../public/fonts/Switzer/WEB/fonts/Switzer-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Switzer/WEB/fonts/Switzer-Extralight.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Switzer/WEB/fonts/Switzer-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Switzer/WEB/fonts/Switzer-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../public/fonts/switzer/Fonts/WEB/fonts/Switzer-Bold.woff2",
+      path: "../../public/fonts/Switzer/WEB/fonts/Switzer-Bold.woff2",
       weight: "700",
       style: "normal",
     },
