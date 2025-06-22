e55f69d7e7a5ec7d5b0f1857f824323e08746ced
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-06-21 22:22:29 -0700

    import abstract line shapes as svg

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,24 @@
 import BackgroundContainer from "@/app/components/BackgroundContainer";
 import ProjectListItem from "@/app/components/ProjectListItem";
-
+import Image from "next/image";
 export default function Home() {
   return (
     <>
       <BackgroundContainer color="neutral" shade="900" textColor="light">
         <div className="hero container flex flex-justify-center flex-align-center">
+          <Image
+            priority
+            src="/abstractLines1.svg"
+            alt="abstract lines"
+            width={1512}
+            height={1113}
+          ></Image>
           <div>
             <h1 className="fs-1000 fw-700">Nick Hibbits</h1>
-            <h2 className="fs-800 "> Web Developer</h2>
+            <h2 className="fs-800 ">Web Developer, Digital Creative</h2>
             <p className="fs-600">
-              Frontend engineer specializing in web-based applications
+              Frontend focused full-stack engineer, specializing in web-app
+              development
             </p>
           </div>
         </div>
@@ -51,6 +59,13 @@ export default function Home() {
       </BackgroundContainer>
       <BackgroundContainer color="primary" shade="500" textColor="light">
         <div className="container">
+          <Image
+            priority
+            src="/abstractLines2.svg"
+            alt="abstract lines"
+            width={1512}
+            height={1113}
+          ></Image>
           <div className="contact-container flex flex-column ">
             <ul className="more-info-links flex">
               <li className="link-group">
