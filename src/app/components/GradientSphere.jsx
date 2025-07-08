094a5b3ecd3932c525ac199e28ad1a4c71ab9a6c
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-07-08 13:45:43 -0700

    add gradient sphere component

---
+++ src/app/components/GradientSphere.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function GradientSphere() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="540"
+      height="540"
+      viewBox="0 0 540 540"
+      fill="none"
+    >
+      <path
+        d="M540 270C540 419.117 419.117 540 270 540C120.883 540 0 419.117 0 270C0 120.883 120.883 0 270 0C419.117 0 540 120.883 540 270Z"
+        fill="url(#paint0_linear_4_4)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_4_4"
+          x1="497.5"
+          y1="147.5"
+          x2="27"
+          y2="366"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#001236" />
+          <stop offset="0.105769" stop-color="#340242" />
+          <stop offset="1" stop-color="#DB761D" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+
+export default GradientSphere;
--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 import BackgroundContainer from "@/app/components/BackgroundContainer";
-import Image from "next/image";
+import GradientSphere from "@/app/components/GradientSphere";
 
 function Hero() {
   return (
     <BackgroundContainer color="neutral" shade="900" textColor="light">
       <div className="hero container flex flex-justify-center flex-align-center">
         <div className="hero-content">
+          <GradientSphere />
           <div className="hero-animated-text">
             <h1 className="fw-700">NICK HIBBITS</h1>
             <p className="hero-subtitle">
