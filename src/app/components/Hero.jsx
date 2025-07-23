1dac039d69dc242417df8170e4794032f129ec24
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-07-22 23:45:29 -0700

    edit gradient circle style and inital props

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
       <div className="hero container flex flex-justify-center flex-align-center">
         <div className="hero-content">
           <GradientSphere size={540} />
-          <GradientSphere size={200} initialVelocity={{ x: 2, y: 1.5 }} />
+          <GradientSphere size={200} initialVelocity={{ x: 2.5, y: 2 }} />
           <div className="hero-animated-text">
             <h1 className="fw-700">NICK HIBBITS</h1>
             <p className="hero-subtitle">
