5e0bf7a4085bf78161211a3cdf1555fe66717426
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-09-24 21:42:35 -0700

    install motion, test cursor

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Cursor } from "motion-plus/react";
+
 import Hero from "@/app/components/Hero";
 import Contact from "@/app/components/Contact";
 import Work from "@/app/components/Work";
@@ -8,6 +10,7 @@ import About from "@/app/components/About";
 export default function Home() {
   return (
     <>
+      <Cursor style={{ borderRadius: 5 }} />
       <Hero />
       <About />
       <Work />
