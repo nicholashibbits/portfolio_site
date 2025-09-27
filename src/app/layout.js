8387d77c548ea202fe8848c708fd158162b72c84
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-09-26 19:26:19 -0700

    refactor pointer, add cursor rules for motion plus

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { switzer } from "./fonts";
 import "../styles/main.scss";
 
+import { Cursor } from "motion-plus/react";
+
 export const metadata = {
   title: "Nick Hibbits",
   description: "Web development portfolio site for Nick Hibbits",
@@ -9,6 +11,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Cursor
+        style={{ width: 5, height: 5 }}
+        matchTextSize={false}
+        className="cursor"
+      />
+      <Cursor
+        follow
+        center={{ x: 0.5, y: 0.5 }}
+        spring={{ stiffness: 1000, damping: 50 }}
+        magnetic={{ snap: 0.9, padding: 0 }}
+        matchTextSize={false}
+        style={{ width: 40, height: 40, borderRadius: 200 }}
+        variants={{
+          magnetic: { opacity: 0 },
+        }}
+        className="reticule"
+      />
+
       <body className={switzer.variable}>{children}</body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { Cursor } from "motion-plus/react";
-
 import Hero from "@/app/components/Hero";
 import Contact from "@/app/components/Contact";
 import Work from "@/app/components/Work";
@@ -10,19 +8,6 @@ import About from "@/app/components/About";
 export default function Home() {
   return (
     <>
-      <Cursor style={{ width: 5, height: 5 }} className="cursor" />
-      <Cursor
-        follow
-        center={{ x: 0.5, y: 0.5 }}
-        spring={{ stiffness: 1000, damping: 50 }}
-        magnetic={{ snap: 0.9, padding: 0 }}
-        style={{ width: 40, height: 40, borderRadius: 200 }}
-        variants={{
-          magnetic: { opacity: 0 },
-        }}
-        className="reticule"
-      />
-
       <Hero />
       <About />
       <Work />
