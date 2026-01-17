b1f7cdbf163126dde98939252c2291bb25ee0f56
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2026-01-16 19:02:02 -0600

    refactor layout, add box-shadow animation

--- src/app/components/ProjectListItem.jsx
+++ src/app/components/ProjectListItem.jsx
@@ -27,13 +27,50 @@ function ProjectListItem({
         >
       <motion.div
         className="project-item-content-inner"
-        animate={({
-          maxHeight: isHovered ? "200px" : "140px",
-        })}
-
+        animate={{
+          maxHeight: isHovered ? "300px" : "140px",
+          boxShadow: isHovered 
+            ? "-18px 17px 0px 0px rgba(0,0,0,1)"
+            : "none",
+        }}
+        transition={{
+          maxHeight: { duration: 0.3 },
+          boxShadow: { duration: 0.3, ease: "easeInOut" },
+        }}
       >
-        <h2 className="fs-700 card-header">{project}</h2>
-        <p className="fs-400">{description}</p>
+        <motion.h2 
+          className="fs-700 card-header"
+          style={{
+            transformOrigin: "left center",
+          }}
+          animate={{
+            scale: isHovered ? 1.1 : 1,
+          }}
+          transition={{
+            duration: 0.3,
+            ease: "easeInOut",
+          }}
+        >{project}</motion.h2>
+        <motion.p 
+          className="fs-400"
+          style={{
+            transformOrigin: "left center",
+          }}
+          animate={{
+            scale: isHovered ? 1.05 : 1,
+            paddingRight: isHovered ? "10px" : "0",
+          }}
+          transition={{
+            scale: {
+
+              duration: 0.3,
+              ease: "easeInOut",
+            },
+            paddingRight: {
+              duration: 0,
+            }
+          }}
+        >{description}</motion.p>
         <motion.p className="fs-400 margin-block-start-12 extended-description"
         animate={{
           transform: isHovered ? "translateY(0)" : "translateY(-100%)",
