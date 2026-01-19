fa2b98a48fbaa97de42af92dae7fa1dba9d26d78
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2026-01-19 13:31:06 -0600

    re-enable zoom effect on work card hover

--- src/app/components/ProjectListItem.jsx
+++ src/app/components/ProjectListItem.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import { motion} from "motion/react";
 
 function ProjectListItem({
