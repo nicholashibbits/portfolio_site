71bb7da48233cc86de76cace5b8ffa33b72b9191
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2026-01-22 16:50:17 -0600

    wip

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -16,25 +16,36 @@ function useParallax(value, distance) {
 function ImageWithText() {
     const ref = useRef(null)
     const { scrollYProgress } = useScroll({ target: ref })
-    const y = useParallax(scrollYProgress, 220)
+    const y = useParallax(scrollYProgress, 320)
+    const opacity = useTransform(scrollYProgress, [0, 0.5, 1], [0.3, 1, 0.3])
 
     return (
-        <section className="img-container">
+        <section className="img-container container">
             <div ref={ref}>
                 <Image
                     src="/orange-headshot.jpg"
                     alt="Nick Hibbits"
-                    width={500}
-                    height={500}
+                    width={600}
+                    height={1000}
                     className="about-image"
                 />
             </div>
-            <motion.h2
+            <motion.div
                 // Hide until scroll progress is measured
                 initial={{ visibility: "hidden" }}
                 animate={{ visibility: "visible" }}
-                style={{ y }}
-            >Web Engineer</motion.h2>
+                style={{ y, opacity }}
+                className="about-text"
+            >
+               <h2>Web Engineer</h2>
+               <p>Create and steward web-based ecosystems</p>
+               <ul>
+               <li>- Experienced with both code-based and low/no-code website building solutions.</li>
+               <li>- Maintained and contributed to enterprise-level codebases in both startup and corporate environments.</li>
+               <li>- Adaptive systems thinker who loves a slick UI.</li>
+                </ul>
+              
+              </motion.div>
         </section>
     )
 }
