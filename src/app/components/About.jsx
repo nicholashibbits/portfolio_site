e4c2281558d0b6cefe9ee817302063ded2c72fcd
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2026-03-17 21:44:10 -0500

    postioin bio section for menu nav

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -25,55 +25,57 @@ function ImageWithText() {
 
   const yRaw = useTransform(
     scrollYProgress,
-    [0.15, 0.4, 0.7, 0.9, 1],
-    [200, restY, restY, -100, -120],
+    [0.15, 0.25, 0.4, 0.7, 0.9, 1],
+    [200, 0, restY, restY, -100, -120],
   );
 
   const y = useSpring(yRaw, { stiffness: 100, damping: 15 });
 
   const opacity = useTransform(
     scrollYProgress,
-    [0.1, 0.2, 0.6, 0.7, .9, 0.98],
-    [0, 0.7, 1, 1, 1, 0],
+    [0.1, 0.2, 0.6, 0.7, 0.9, 0.98],
+    [0, 0.9, 1, 1, 1, 0],
   );
 
   return (
     <div className="img-container" ref={ref}>
-      <motion.div
-        animate={{ y: [0, -6, 2, -4, 0], x: [0, 3, -2, 4, 0] }}
-        transition={{
-          duration: 8,
-          repeat: Infinity,
-          ease: [0.45, 0, 0.55, 1],
-        }}
-        style={{ willChange: "transform" }}
-      >
-        <Image
-          src="/orange-headshot.jpg"
-          alt="Nick Hibbits"
-          width={600}
-          height={1000}
-          className="about-image"
-        />
-      </motion.div>
-      <motion.div
-        style={{ y, opacity, willChange: "transform, opacity" }}
-        className="about-text"
-      >
-        <h2>Sound check </h2>
-        <p>
-          I'm a web developer who cares about how digital experiences{" "}
-          <span
-            className="gradient-text fs-500"
-            style={{ fontStyle: "italic" }}
-          >
-            feel{" "}
-          </span>{" "}
-          as much as how they function. I come at design problems from a
-          background in music production where timing, space, and texture matter
-          as much as structure.
-        </p>
-      </motion.div>
+      <div className="img-wrapper">
+        <motion.div
+          animate={{ y: [0, -6, 2, -4, 0], x: [0, 3, -2, 4, 0] }}
+          transition={{
+            duration: 8,
+            repeat: Infinity,
+            ease: [0.45, 0, 0.55, 1],
+          }}
+          style={{ willChange: "transform" }}
+        >
+          <Image
+            src="/orange-headshot.jpg"
+            alt="Nick Hibbits"
+            width={600}
+            height={1000}
+            className="about-image"
+          />
+        </motion.div>
+        <motion.div
+          style={{ y, opacity, willChange: "transform, opacity" }}
+          className="about-text"
+        >
+          <h2>Sound check </h2>
+          <p>
+            I'm a web developer who cares about how digital experiences{" "}
+            <span
+              className="gradient-text fs-500"
+              style={{ fontStyle: "italic" }}
+            >
+              feel{" "}
+            </span>{" "}
+            as much as how they function. I come at design problems from a
+            background in music production where timing, space, and texture
+            matter as much as structure.
+          </p>
+        </motion.div>
+      </div>
     </div>
   );
 }
